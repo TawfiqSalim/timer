e7ec0e1e3f9fec0b3d25f61f49beefed3c4eae7f
TawfiqSalim <85137519+TawfiqSalim@users.noreply.github.com>
2021-11-07 00:23:34 +0200

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,1 +1,50 @@
-!function(i){var e={init:function(e){var t={timer:null,timerSeconds:50,callback:function(){},timerCurrent:0,showPercentage:!1,fill:!1,color:"#CCC"};return t=i.extend(t,e),this.each(function(){var e=i(this);e.data("pietimer")||(e.addClass("pietimer"),e.css({fontSize:e.width()}),e.data("pietimer",t),t.showPercentage&&e.find(".percent").show(),t.fill&&e.addClass("fill"),e.pietimer("start"))})},stopWatch:function(){var e=i(this).data("pietimer");if(e){var t=(e.timerFinish-(new Date).getTime())/1e3;if(t<=0)clearInterval(e.timer),i(this).pietimer("drawTimer",100),e.callback();else{var r=100-t/e.timerSeconds*100;i(this).pietimer("drawTimer",r)}}},drawTimer:function(e){$this=i(this);var t=$this.data("pietimer");if(t){$this.html('<div class="percent"></div><div class="slice'+(e>50?' gt50"':'"')+'><div class="pie"></div>'+(e>50?'<div class="pie fill"></div>':"")+"</div>");var r=3.6*e;$this.find(".slice .pie").css({"-moz-transform":"rotate("+r+"deg)","-webkit-transform":"rotate("+r+"deg)","-o-transform":"rotate("+r+"deg)",transform:"rotate("+r+"deg)"}),$this.find(".percent").html(Math.round(e)+"%"),t.showPercentage&&$this.find(".percent").show(),$this.hasClass("fill")?$this.find(".slice .pie").css({backgroundColor:t.color}):$this.find(".slice .pie").css({borderColor:t.color})}},start:function(){var e=i(this).data("pietimer");e&&(e.timerFinish=(new Date).getTime()+1e3*e.timerSeconds,i(this).pietimer("drawTimer",0),e.timer=setInterval("$this.pietimer('stopWatch')",50))},reset:function(){var e=i(this).data("pietimer");e&&(clearInterval(e.timer),i(this).pietimer("drawTimer",0))}};i.fn.pietimer=function(t){return e[t]?e[t].apply(this,Array.prototype.slice.call(arguments,1)):"object"!=typeof t&&t?void i.error("Method "+t+" does not exist on jQuery.pietimer"):e.init.apply(this,arguments)}}(jQuery);
+  <script>
+//<![CDATA[
+$(document).ready(function () {
+    var idBlog = "8067173251129912712";
+    $(function () {
+        "use strict";
+        $.ajax({
+            dataType: "json",
+            url: "https://www.blogger.com/feeds/" + idBlog + "/posts/default?alt=json-in-script",
+            method: "GET",
+            dataType: "jsonp",
+            success: function (e) {
+                var t;
+                for (t = 0; t < e.feed.entry.length; t += 1) {
+                    var n = $(e.feed.entry[t].content.$t);
+                    if (0 === t && !$("body").hasClass("error_page")) {
+                        for (var o = n.find("li"), d = [], a = 0; a < o.length; a += 1) d.push($(o[a]).text());
+                        var r,
+                            i = window.location.hostname.toLowerCase(),
+                            f = window.location.href.toLowerCase(),
+                            s = d.length - 1;
+                        for (r = 0; r < d.length; r += 1) {
+                            if (-1 != i.indexOf(d[r])) {
+                                var l = $(e.feed.entry[t].content.$t).find("script"),
+                                    p = $(e.feed.entry[t].content.$t).find("style");
+                                $("head").append(p), $("head").append(l);
+                                break;
+                            }
+                            r == s &&
+                                -1 == f.indexOf("post-preview") &&
+                                -1 == f.indexOf("www.blogger") &&
+                                -1 == f.indexOf("b/layout-preview") &&
+                                -1 == f.indexOf("b/preview") &&
+                                -1 == f.indexOf("translate.google") &&
+                                -1 == f.indexOf("webcache.googleusercontent") &&
+                                -1 == f.indexOf("template-editor") &&
+                                $("html").html(n.find(".redirect").html());
+                        }
+                    }
+                    if (1 === t) {
+                        p = $(e.feed.entry[t].content.$t).find("style");
+                        $("head").append(p);
+                    }
+                }
+            },
+        });
+    });
+});
+//]]>
+</script>
